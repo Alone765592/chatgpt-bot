bceabc947c9c4449db221b4415b730d51793ff94
Alonehubb <momovag764@yahoo.com>
2024-12-22 09:41:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,15 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = environ.get("API_ID" , "2099007")
-API_HASH = environ.get("API_HASH" , "b1b3d7b3b1b3d7b3b1b3d7b3b1b3d7b3")
+API_ID = environ.get("API_ID" , "20901045")
+API_HASH = environ.get("API_HASH" , "dec03cafafbd892b285499762a896082")
 BOT_TOKEN = environ.get("BOT_TOKEN" , "2099007:AAH1b3d7b3b1b3d7b3b1b3d7b3b1b3d7b3")
-ADMIN = int(environ.get("ADMIN" , "1847899007"))
-CHAT_GROUP = int(environ.get("CHAT_GROUP", "-100"))
-LOG_CHANNEL = environ.get("LOG_CHANNEL", "-100")
-MONGO_URL = environ.get("MONGO_URL" , "mongodb://localhost:27017")
+ADMIN = int(environ.get("ADMIN" , "6324457826"))
+CHAT_GROUP = int(environ.get("CHAT_GROUP", "-1002473045711"))
+LOG_CHANNEL = environ.get("LOG_CHANNEL", "-1002447343432")
+MONGO_URL = environ.get("MONGO_URL" , "mongodb+srv://Alone:Sofihub76@cluster0.3bfyy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 AUTH_CHANNEL = int(
-    environ.get("AUTH_CHANNEL", "-1001734958816")
+    environ.get("AUTH_CHANNEL", "-1002199891363")
 )
 FSUB = environ.get("FSUB", True)
 STICKERS_IDS = (
